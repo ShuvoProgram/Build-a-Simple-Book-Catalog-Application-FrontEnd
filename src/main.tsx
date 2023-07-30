2cc77c800015983c891e9f2e7cf821858e574a19
Shuvo Khan <shuvoprogramer@gmail.com>
2023-07-30 19:10:25 +0600

    fix page load bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
 import { RouterProvider } from 'react-router-dom';
 import './index.css'
+import routes from './routes/routes';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
